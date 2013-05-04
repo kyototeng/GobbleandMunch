c44a4cdf4e476fa03b380ef85eebe889e748349b
kyototeng <agfdsh@example.com>
2013-05-04 21:07:18 +0100

    .

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/GobbleandMunch/"
+http_images_path = "/GobbleandMunch/images"
+http_fonts_path = "/GobbleandMunch/fonts"
+css_dir = "public/GobbleandMunch/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
